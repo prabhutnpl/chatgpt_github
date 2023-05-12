d301b7cf8b68d829fcb01cc0358716b4529891d9
Prabhu <prabhutnpl@live.com>
2023-05-13 00:34:59 +0530

    readme and llm changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from llama_index import GPTVectorStoreIndex, GithubRepositoryReader
+from llama_index import GPTVectorStoreIndex, GithubRepositoryReader, GPTKeywordTableIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
+from langchain import OpenAI
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -11,8 +12,8 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 github_token = os.environ["GITHUB_TOKEN"]
 
 owner = "prabhutnpl"
-repo = "chatgpt"
-branch = "main"
+repo = "chatgpt_github"
+branch = "master"
 
 documents = GithubRepositoryReader(
     github_token=github_token,
@@ -22,7 +23,23 @@ documents = GithubRepositoryReader(
     verbose=False,
 ).load_data(branch=branch)
 
-my_index = GPTVectorStoreIndex.from_documents(documents)
+# define prompt helper
+# set maximum input size
+max_input_size = 4096
+# set number of output tokens
+num_output = 256
+# set maximum chunk overlap
+max_chunk_overlap = 20
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+# define LLM
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-002", max_tokens=num_output))
+
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+# build index
+my_index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+
 
 @app.route('/')
 def index():
